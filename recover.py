c31bc857bf573de6a215cac56749ef0f4b3dfb39
Prince Bhardwaj <81508677+p21nc3@users.noreply.github.com>
2024-03-04 09:46:02 +0530

    Algorithm - encode + decode

---
+++ recover.py
@@ -0,0 +1,24 @@
+from pyseltongue import PlaintextToHexSecretSharer
+import inquirer
+import animation
+
+def main():
+    shares = [input('Enter your share: ')]
+    while True:
+        questions = [
+            inquirer.List('share',
+                        message='Enter next share',
+                        choices=['OK', 'I don\'t have another one'],
+                    ),
+        ]
+        answer = inquirer.prompt(questions)['share']
+        if answer != 'OK':
+            break
+        shares.append(input('Enter your share: '))
+    wait = animation.Wait('spinner', 'Generating randomness.. It may take a while.. ')
+    wait.start()
+    message = PlaintextToHexSecretSharer.recover_secret(shares)
+    wait.stop()
+    print('Original message:\n'+message)
+if __name__ == '__main__':
+	main()
---
+++ share.py
@@ -0,0 +1,70 @@
+from pyseltongue import PlaintextToHexSecretSharer
+import pyqrcode
+import inquirer
+import animation
+
+def main():
+    questions = [
+        inquirer.List('parties',
+                    message='How many shares do you want?',
+                    choices=['2', '3', '4', 'other'],
+                ),
+    ]
+    answer = inquirer.prompt(questions)
+    if answer['parties'] == 'other':
+        parties = int(input('Type a number: '))
+        while parties < 2:
+            parties = int(input('Type a number greater than 1: '))
+    else:
+        parties = int(answer['parties'])
+    if parties > 2:
+        min_threshold = 2
+        max_threshold = parties
+        thresholds = [x for x in range(min_threshold, max_threshold+1)]
+        questions = [
+            inquirer.List('threshold',
+                        message='How many shares should be enough for decryption? (Most secure: ' + str(max_threshold) + ')',
+                        choices=thresholds,
+                    ),
+        ]
+        answer = inquirer.prompt(questions)
+        threshold = int(answer['threshold'])
+    else:
+        threshold = parties
+    questions = [
+        inquirer.List('format',
+                    message='Select the format of output images',
+                    choices=['png', 'svg', 'terminal'],
+                ),
+    ]
+    format = inquirer.prompt(questions)['format']
+    if format != 'terminal':
+        questions = [
+            inquirer.List('scale',
+                        message='Size of output images',
+                        choices=['Small', 'Medium', 'Large'],
+                    ),
+        ]
+        answers = inquirer.prompt(questions)
+        if answers['scale'] == 'Small':
+            scale = 2
+        elif answers['scale'] == 'Medium':
+            scale = 4
+        elif answers['scale'] == 'Large':
+            scale = 8
+    secret = input('Enter your message: ')
+    wait = animation.Wait('spinner', 'Generating randomness.. It may take a while.. ')
+    wait.start()
+    shares = PlaintextToHexSecretSharer.split_secret(secret, threshold, parties)
+    wait.stop()
+    print(shares)
+    for share in shares: 
+        img = pyqrcode.create(share)
+        if format == 'png':
+            img.png(share[0]+'.png', scale = scale)
+        elif format == 'svg':
+            img.svg(share[0]+'.svg', scale = scale)
+        elif format == 'terminal':
+            print(img.terminal())
+if __name__ == '__main__':
+	main()
